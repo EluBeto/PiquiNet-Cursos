135b2cde0e13f004825cb98533641e5786bfd96d
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2020-12-03 11:44:09 -0600

    Initializing project

---
+++ VUE/Holamundo/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            titulo: "Mi banco con VUE.Js",
+            cantidad: 0,
+            enlace: 'https://v3.vuejs.org/guide/installation.html#vue-devtools',
+            estado: false,
+            servicios: ['transferencias', 'pagos', 'giros', 'cheques']
+        }
+    }
+})
