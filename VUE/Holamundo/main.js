1fac8bd808006bc71166aae9bb6c67ba80e0a854
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2020-12-03 18:05:30 -0600

    Pasar parametros a methos y desabilitar boton

--- VUE/Holamundo/main.js
+++ VUE/Holamundo/main.js
@@ -2,15 +2,25 @@ const app = Vue.createApp({
     data(){
         return{
             titulo: "Mi banco con VUE.Js",
-            cantidad: 0,
+            cantidad: 1000,
             enlace: 'https://v3.vuejs.org/guide/installation.html#vue-devtools',
             estado: false,
-            servicios: ['transferencias', 'pagos', 'giros', 'cheques']
+            servicios: ['transferencias', 'pagos', 'giros', 'cheques'],
+            desactivar: false
         }
     },
     methods: {
         agregarSaldo(){
             this.cantidad = this.cantidad + 100
+        },
+        restaSaldo(valor){
+            if(this.cantidad === 0){
+                this.desactivar = true
+                alert("Saldo en Cero")
+               
+                return
+            }
+            this.cantidad = this.cantidad - valor
         }
     }
 })
