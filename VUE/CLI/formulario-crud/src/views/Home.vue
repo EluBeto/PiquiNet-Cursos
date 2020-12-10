9cadb2f58748fd40c7e6d77d7b7529b56602d672
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2020-12-09 22:44:29 -0600

    Vuex y generar IDs

--- VUE/CLI/formulario-crud/src/views/Home.vue
+++ VUE/CLI/formulario-crud/src/views/Home.vue
@@ -8,6 +8,9 @@
 
 <script>
 import Input from '../components/Input'
+import { mapActions } from "vuex"
+const shortid = require('shortid')
+
 export default {
   name: 'Home',
   components: {
@@ -25,6 +28,7 @@ export default {
     }
   },
   methods:{
+    ...mapActions(['setTareas']),
     procesarForm(){
       console.log(this.tarea)
       if (this.tarea.nombre.trim() === "") {
@@ -32,7 +36,15 @@ export default {
         return
       }
       console.log('Se guardo')
+      
+      //Generar ID
+      this.tarea.id = shortid.generate()
+      console.log(this.tarea.id)
+      // Enviar datos
+      this.setTareas(this.tarea)
+
       this.tarea = {
+        id: '',
         nombre: '',
         categorias: [],
         estado: '',
