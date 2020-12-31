231c9f53ee8109f903005705f8d84cce7cc7d915
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2020-12-30 19:09:35 -0600

    Primeros metodos de Composition API

--- VUE/Composition API/composition/src/views/About.vue
+++ VUE/Composition API/composition/src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Contador: {{ contador }}</h1>
   </div>
+  <hr>
+  <button
+  class="btn btn-success"
+   @click="aumentar"
+  >+</button>
+  <button
+  class="btn btn-danger"
+   @click="disminuir"
+  >-</button>
 </template>
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const contador = ref(0)
+
+    const aumentar = () => {
+      contador.value ++
+    }
+
+    const disminuir = () => {
+      contador.value --
+    }
+    
+    return {
+      contador,
+      aumentar,
+      disminuir
+    }
+  }
+}
+</script>
--- VUE/Composition API/composition/src/views/Home.vue
+++ VUE/Composition API/composition/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Contador: {{ contador }}</h1>
+    <hr>
+    <button class="btn btn-success"
+      @click="suma"
+    >
+      +
+    </button>
+    
+    <button class="btn btn-danger"
+      @click="restar"
+    >
+      -
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    suma() {
+      this.contador ++
+    },
+    restar() {
+      this.contador --
+    }
   }
 }
 </script>
