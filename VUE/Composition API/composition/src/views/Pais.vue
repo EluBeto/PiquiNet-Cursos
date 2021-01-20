f57895e8e1559df304559becb6f34c02693891b4
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2021-01-19 18:49:05 -0600

    Params en setup()

---
+++ VUE/Composition API/composition/src/views/Pais.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <h1>Detalle: {{ $route.params.nombre }}</h1> -->
+  <h1>Detalle: {{ nombre }}</h1>
+</template>
+
+<script>
+import {useRoute} from 'vue-router'
+export default {
+    props: ['nombre'],
+    setup(props) {
+        console.log(props.nombre)
+        const nombreParams = useRoute()
+        console.log(nombreParams.params.nombre)
+    }
+}
+</script>
--- VUE/Composition API/composition/src/views/Paises.vue
+++ VUE/Composition API/composition/src/views/Paises.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Lista de Paises</h1>
   <p v-for="(pais, index) in arrayData" :key="index">
-      {{ pais.name }}
+      <router-link :to="`/paises/${pais.name}`">
+          {{ pais.name }}
+      </router-link>
   </p>
 </template>
 
