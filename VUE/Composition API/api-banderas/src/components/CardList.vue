97da0cf9fcb384468ea284d6252aa74f98a25c55
Edilberto Lopez Ubaldo <ing.edilberto.lu@hotmail.com>
2021-01-20 20:08:08 -0600

    Composition API - Actions

---
+++ VUE/Composition API/api-banderas/src/components/CardList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+      <div class="col-12">
+          Card
+      </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+export default {
+    setup() {
+        const store = useStore()
+        onMounted(() => {
+            store.dispatch('getPaises')
+            // store.state.paises
+        })
+    }
+}
+</script>
--- VUE/Composition API/api-banderas/src/store/index.js
+++ VUE/Composition API/api-banderas/src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    paises: [],
+    paisesFiltrados: []
   },
   mutations: {
+    setPaises(state, payload) {
+      state.paises = payload
+    },
+    setPaisesFiltrados(state, payload) {
+      state.paisesFiltrados = payload
+    }
   },
   actions: {
+    async getPaises({ commit }) {
+      try {
+        const responsePaises = await fetch('api.json')
+        const data = await responsePaises.json()
+        commit('setPaises', data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
